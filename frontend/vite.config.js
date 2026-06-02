1af0762e57638eafb26db4bdc92e50679e29b0ab
Raul Díaz Díaz <126184231+Raul-dd@users.noreply.github.com>
2026-06-02 00:50:58 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     allowedHosts: true,
     proxy: {
       '/api': {
-        target: 'http://0.0.0.0:5001',
+        target: 'http://127.0.0.1:5001',
         changeOrigin: false,
         secure: false,
         ws: true
